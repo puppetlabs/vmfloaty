832169a0667732ad85f9b2577ab2c2432a9b517d
Brian Cain <brianccain@gmail.com>
2015-09-14 21:53:40 -0700

    Check if url is nil before making connection

--- lib/vmfloaty/http.rb
+++ lib/vmfloaty/http.rb
@@ -2,6 +2,11 @@ require 'faraday'
 
 class Http
   def self.get_conn(verbose, url)
+    if url.nil?
+      STDERR.puts "The url you provided was empty"
+      exit 1
+    end
+
     conn = Faraday.new(:url => url) do |faraday|
       faraday.request :url_encoded
       faraday.response :logger if verbose
