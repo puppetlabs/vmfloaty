cecea8bc052b28fe28a068bd7054ae6ae997867b
Tim Sharpe <tim@bombasticmonkey.com>
2019-02-03 12:47:16 +1100

    (rubocop) Fix Layout/SpaceAfterNot

--- lib/vmfloaty.rb
+++ lib/vmfloaty.rb
@@ -52,7 +52,7 @@ class Vmfloaty
 
         max_pool_request = 5
         large_pool_requests = os_types.select { |_,v| v > max_pool_request }
-        if ! large_pool_requests.empty? && ! force
+        if !large_pool_requests.empty? && !force
           STDERR.puts "Requesting vms over #{max_pool_request} requires a --force flag."
           STDERR.puts 'Try again with `floaty get --force`'
           exit 1
